4ec0f59a015a42e278e373565470b7b0f9c1c368
ElyesMaamouri <88774647+ElyesMaamouri@users.noreply.github.com>
2025-11-29 17:52:27 +0100

    add url server backend

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://backend-invoice-generator.onrender.com/";
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
